e7eb86b2a3288d914f02ecc42e4f2f595cda20c4
1405939594 <1405939594@qq.com>
2020-04-24 18:01:08 +0800

    把本地工程添加到git

---
+++ git-test/src/com/sakura/git/FirstClass.java
@@ -0,0 +1,13 @@
+package com.sakura.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("111");
+        System.out.println("222");
+        System.out.println("333");
+        System.out.println("444");
+        System.out.println("555");
+        System.out.println("666");
+        System.out.println("777");
+    }
+}
